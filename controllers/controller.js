2a47acc80eb6e314fc7e542a7683fbe93e587295
Marta Casal Tàpia <>
2020-11-23 20:09:09 +0100

    fixed the checking wheels and adding wheels

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,49 +1,61 @@
 "use strict";
 // Es declara la variable car com a objecte (car.js) però encara sense definir
 var car;
-var plate = document.getElementById('plate');
-var brand = document.getElementById('brand');
-var color = document.getElementById('color');
 var infoCar = document.getElementById('infoCar');
-var formWheels = document.getElementById('formWheels');
 var infoWheels = document.getElementById('infoWheels');
+var formWheels = document.getElementById('formWheels');
 function createCar() {
+    //les definicions que no siguin globals, van dins de la funció corresponent
+    var plate = document.getElementById('plate').value;
+    var brand = document.getElementById('brand').value;
+    var color = document.getElementById('color').value;
     //primer valida que tots els camps siguin correctes
     var validateInfo = validate(plate, brand, color);
-    if (validateInfo == 0) {
-        car = new Car(plate.value, brand.value, color.value);
+    if (validateInfo === 0) {
+        car = new Car(plate, brand, color);
         infoCar.classList.remove("d-none");
-        infoCar.innerHTML = "<p class=\"pt-3\"><span class=\"font-weight-bold\">Plate:</span> " + plate.value + "</p>\n        <p class=\"pt-3\"><span class=\"font-weight-bold\">Brand:</span> " + brand.value + "</p>\n        <p class=\"pt-3\"><span class=\"font-weight-bold\">Color:</span> " + color.value + "</p>";
+        infoCar.innerHTML = "<p class=\"pt-3\"><span class=\"font-weight-bold\">Plate:</span> " + plate + "</p>\n        <p class=\"pt-3\"><span class=\"font-weight-bold\">Brand:</span> " + brand + "</p>\n        <p class=\"pt-3\"><span class=\"font-weight-bold\">Color:</span> " + color + "</p>";
         formWheels.classList.remove("d-none");
     }
 }
 function addWheels() {
-    // valida el diametre de les rodes
+    // valida el diametre de les rodes i que els camps no estiguin buits
     infoWheels.innerHTML = "";
     infoWheels.classList.remove("d-none");
+    var checkWheels = false;
     for (var i = 1; i <= 4; i++) { // i = 1 pq comença a la roda 1 fins la 4
-        var wheelDiameter = document.getElementById('wheelDiam' + [i]);
-        var wheelBrand = document.getElementById('wheelBrand' + [i]).value;
-        var diameterValue = parseFloat(wheelDiameter.value); // el valor passa de string a number amb decimals (sempre amb punt, mai amb coma)
-        var validateDiameter = validateWheels(diameterValue, i, wheelBrand);
-        if (validateDiameter == 0) {
-            infoWheels.innerHTML += "<p class=\"col-12 pt-3 font-weight-bold text-uppercase text-center\">Wheel " + i + "</p><p class=\"col-6 pt-3 text-center\">Brand: " + wheelBrand + "</p><p class=\"col-6 pt-3 text-center\">Diameter: " + diameterValue + "</p>";
-            car.addWheel(new Wheel(diameterValue, wheelBrand));
+        var wheelDiameter = document.getElementById('wheelDiam' + [i]).value; // OJO DUPLICAT!!
+        var wheelBrand = document.getElementById('wheelBrand' + [i]).value; // OJO DUPLICAT!!
+        var diameterValue = Number(wheelDiameter); // el valor passa de string a number amb decimals (sempre amb punt, mai amb coma).
+        var validateWheel = validateWheels(diameterValue, i, wheelBrand);
+        if (validateWheel > 0 && !checkWheels) {
+            checkWheels = true;
         }
     }
-    //console.log(car);
+    // això es veurà quan no hi hagin errors al validateWheels
+    if (!checkWheels) {
+        for (var i = 1; i <= 4; i++) {
+            var wheelDiameter = document.getElementById('wheelDiam' + [i]).value; // OJO DUPLICAT!!
+            var wheelBrand = document.getElementById('wheelBrand' + [i]).value; // OJO DUPLICAT!!
+            var diameterValue = Number(wheelDiameter); // OJO DUPLICAT!!
+            car.addWheel(new Wheel(diameterValue, wheelBrand));
+            //infoWheels.innerHTML += `<p class="col-12 pt-3 font-weight-bold text-uppercase text-center">Wheel ${i}</p><p class="col-6 pt-3 text-right">Brand: ${wheelBrand}</p><p class="col-6 pt-3 text-left">Diameter: ${diameterValue}</p>`;
+            infoWheels.innerHTML = "<p class=\"col-12 pt-3 font-weight-bold text-center text-success\">ALL WHELLS HAVE BEEN SUCCESSFULLY ADDED</p>";
+        }
+        console.log(car);
+    }
 }
 function validate(plate, brand, color) {
     var error = 0;
-    if (plate.value == "") {
+    if (plate == "") {
         infoCar.innerHTML = "<p class=\"text-danger pt-3\">Plate value is required!</p>";
         error++;
     }
-    else if (brand.value == "") {
+    else if (brand == "") {
         infoCar.innerHTML = "<p class=\"text-danger pt-3\">Brand value is required!</p>";
         error++;
     }
-    else if (color.value == "") {
+    else if (color == "") {
         infoCar.innerHTML = "<p class=\"text-danger pt-3\">Color value is required!</p>";
         error++;
     }
@@ -55,21 +67,35 @@ function validate(plate, brand, color) {
 }
 function validatePlate(plate) {
     var regex = /^[0-9]{4}[a-zA-Z]{3}$/;
-    return regex.test(plate.value) ? true : false;
+    return regex.test(plate) ? true : false;
 }
 function validateWheels(diameter, index, brand) {
     var error = 0;
+    var diameterElement = document.getElementById("wheelDiam" + [index]);
+    var brandElement = document.getElementById("wheelBrand" + [index]);
+    var errorDiametre = document.getElementById("errorwheelDiam" + [index]);
+    var errorBrand = document.getElementById("errorwheelBrand" + [index]);
     if (diameter == "") {
-        infoWheels.innerHTML += "<p class=\"col-12 text-danger pt-3 text-center\">Wheel " + index + " diameter is required!</p>";
+        diameterElement.classList.add("is-invalid");
+        errorDiametre.textContent = "Wheel " + index + " diameter is required!";
         error++;
     }
     else if (brand == "") {
-        infoWheels.innerHTML += "<p class=\"col-12 text-danger pt-3 text-center\">Brand for wheel " + index + " is required!</p>";
+        brandElement.classList.add("is-invalid");
+        errorBrand.textContent = "Brand for wheel " + index + " is required!";
         error++;
     }
     else if (diameter < 0.4 || diameter > 2) {
-        infoWheels.innerHTML += "<p class=\"col-12 text-danger pt-3 text-center\">Error value from wheel " + index + ": the diameter had to be between 0,4cm and 2cm.</p>";
+        diameterElement.classList.add("is-invalid");
+        errorDiametre.textContent = "Error value wheel " + index + ": the diameter has to be between 0.4cm and 2cm.";
         error++;
     }
     return error;
+}
+// treu els "is-invalid" del formWheels quan s'introdueixen els nous valors per corregir els erronis
+if (formWheels) {
+    formWheels.addEventListener('blur', function (event) {
+        if (event.target.value != '')
+            event.target.classList.remove('is-invalid');
+    }, true);
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,56 +1,73 @@
 // Es declara la variable car com a objecte (car.js) però encara sense definir
 let car: Car;
 
-let plate = document.getElementById('plate') as HTMLInputElement;
-let brand = document.getElementById('brand') as HTMLInputElement;
-let color = document.getElementById('color') as HTMLInputElement;
 let infoCar = document.getElementById('infoCar') as HTMLElement;
-let formWheels = document.getElementById('formWheels') as HTMLElement;
 let infoWheels = document.getElementById('infoWheels') as HTMLElement;
+let formWheels = document.getElementById('formWheels') as HTMLElement;
 
 function createCar() {
+    //les definicions que no siguin globals, van dins de la funció corresponent
+    let plate = (document.getElementById('plate') as HTMLInputElement).value;
+    let brand = (document.getElementById('brand') as HTMLInputElement).value;
+    let color = (document.getElementById('color') as HTMLInputElement).value;
+
     //primer valida que tots els camps siguin correctes
     let validateInfo = validate(plate, brand, color);
 
-    if (validateInfo == 0) {
-        car = new Car(plate.value, brand.value, color.value);
+    if (validateInfo === 0) {
+        car = new Car(plate, brand, color);
         infoCar.classList.remove("d-none");
 
-        infoCar.innerHTML = `<p class="pt-3"><span class="font-weight-bold">Plate:</span> ${plate.value}</p>
-        <p class="pt-3"><span class="font-weight-bold">Brand:</span> ${brand.value}</p>
-        <p class="pt-3"><span class="font-weight-bold">Color:</span> ${color.value}</p>`;
+        infoCar.innerHTML = `<p class="pt-3"><span class="font-weight-bold">Plate:</span> ${plate}</p>
+        <p class="pt-3"><span class="font-weight-bold">Brand:</span> ${brand}</p>
+        <p class="pt-3"><span class="font-weight-bold">Color:</span> ${color}</p>`;
 
         formWheels.classList.remove("d-none");
     }
 }
 
 function addWheels() {
-    // valida el diametre de les rodes
+    // valida el diametre de les rodes i que els camps no estiguin buits
     infoWheels.innerHTML = "";
     infoWheels.classList.remove("d-none");
+
+    let checkWheels: boolean = false;
+
     for (let i = 1; i <= 4; i++) { // i = 1 pq comença a la roda 1 fins la 4
-        let wheelDiameter = document.getElementById('wheelDiam' + [i]) as HTMLInputElement;
-        let wheelBrand = (document.getElementById('wheelBrand' + [i]) as HTMLInputElement).value;
-        let diameterValue = parseFloat(wheelDiameter.value); // el valor passa de string a number amb decimals (sempre amb punt, mai amb coma)
-        let validateDiameter = validateWheels(diameterValue, i, wheelBrand);
-        if (validateDiameter == 0) {
-            infoWheels.innerHTML += `<p class="col-12 pt-3 font-weight-bold text-uppercase text-center">Wheel ${i}</p><p class="col-6 pt-3 text-center">Brand: ${wheelBrand}</p><p class="col-6 pt-3 text-center">Diameter: ${diameterValue}</p>`;
-            car.addWheel(new Wheel(diameterValue, wheelBrand));
+        let wheelDiameter = (document.getElementById('wheelDiam' + [i]) as HTMLInputElement).value; // OJO DUPLICAT!!
+        let wheelBrand = (document.getElementById('wheelBrand' + [i]) as HTMLInputElement).value; // OJO DUPLICAT!!
+        let diameterValue = Number(wheelDiameter); // el valor passa de string a number amb decimals (sempre amb punt, mai amb coma).
+        let validateWheel: number = validateWheels(diameterValue, i, wheelBrand);
+
+        if (validateWheel > 0 && !checkWheels) {
+            checkWheels = true;
         }
     }
-    //console.log(car);
+
+    // això es veurà quan no hi hagin errors al validateWheels
+    if (!checkWheels) {
+        for (let i = 1; i <= 4; i++) {
+            let wheelDiameter = (document.getElementById('wheelDiam' + [i]) as HTMLInputElement).value; // OJO DUPLICAT!!
+            let wheelBrand = (document.getElementById('wheelBrand' + [i]) as HTMLInputElement).value; // OJO DUPLICAT!!
+            let diameterValue = Number(wheelDiameter); // OJO DUPLICAT!!
+            car.addWheel(new Wheel(diameterValue, wheelBrand));
+            //infoWheels.innerHTML += `<p class="col-12 pt-3 font-weight-bold text-uppercase text-center">Wheel ${i}</p><p class="col-6 pt-3 text-right">Brand: ${wheelBrand}</p><p class="col-6 pt-3 text-left">Diameter: ${diameterValue}</p>`;
+            infoWheels.innerHTML = `<p class="col-12 pt-3 font-weight-bold text-center text-success">ALL WHELLS HAVE BEEN SUCCESSFULLY ADDED</p>`;
+        }
+        console.log(car);
+    }
 }
 
-function validate(plate: HTMLInputElement, brand: HTMLInputElement, color: HTMLInputElement) {
+function validate(plate: string, brand: string, color: string) {
     let error = 0;
 
-    if (plate.value == "") {
+    if (plate == "") {
         infoCar.innerHTML = `<p class="text-danger pt-3">Plate value is required!</p>`
         error++;
-    } else if (brand.value == "") {
+    } else if (brand == "") {
         infoCar.innerHTML = `<p class="text-danger pt-3">Brand value is required!</p>`
         error++;
-    } else if (color.value == "") {
+    } else if (color == "") {
         infoCar.innerHTML = `<p class="text-danger pt-3">Color value is required!</p>`
         error++;
     } else if (!validatePlate(plate)) {
@@ -63,22 +80,37 @@ function validate(plate: HTMLInputElement, brand: HTMLInputElement, color: HTMLI
 
 function validatePlate(plate: any) {
     let regex = /^[0-9]{4}[a-zA-Z]{3}$/;
-    return regex.test(plate.value) ? true : false;
+    return regex.test(plate) ? true : false;
 }
 
 function validateWheels(diameter: any, index: number, brand: any) {
     let error = 0;
 
+    let diameterElement = document.getElementById("wheelDiam" + [index]) as HTMLInputElement;
+    let brandElement = document.getElementById("wheelBrand" + [index]) as HTMLInputElement;
+    let errorDiametre = document.getElementById("errorwheelDiam" + [index]) as HTMLInputElement;
+    let errorBrand = document.getElementById("errorwheelBrand" + [index]) as HTMLInputElement;
+
     if (diameter == "") {
-        infoWheels.innerHTML += `<p class="col-12 text-danger pt-3 text-center">Wheel ${index} diameter is required!</p>`
+        diameterElement.classList.add("is-invalid");
+        errorDiametre.textContent = `Wheel ${index} diameter is required!`
         error++;
     } else if (brand == "") {
-        infoWheels.innerHTML += `<p class="col-12 text-danger pt-3 text-center">Brand for wheel ${index} is required!</p>`
+        brandElement.classList.add("is-invalid");
+        errorBrand.textContent = `Brand for wheel ${index} is required!`
         error++;
     } else if (diameter < 0.4 || diameter > 2) {
-        infoWheels.innerHTML += `<p class="col-12 text-danger pt-3 text-center">Error value from wheel ${index}: the diameter had to be between 0,4cm and 2cm.</p>`
+        diameterElement.classList.add("is-invalid");
+        errorDiametre.textContent = `Error value wheel ${index}: the diameter has to be between 0.4cm and 2cm.`
         error++;
     }
 
     return error;
+}
+
+// treu els "is-invalid" del formWheels quan s'introdueixen els nous valors per corregir els erronis
+if (formWheels) {
+    formWheels.addEventListener('blur', (event: any) => {
+        if (event.target.value != '') event.target.classList.remove('is-invalid');
+    }, true);
 }
